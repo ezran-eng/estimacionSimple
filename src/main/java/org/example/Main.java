35639e9d5c76beef2709fe25dd048f5413c233ff
Ezequiel <eezquieloelero@gmail.com>
2023-04-23 12:54:28 -0300

    Primer Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        int length = input.length();
-        if (length > 100) {
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
-        }
-        scanner.close();
+        
     }
 }
