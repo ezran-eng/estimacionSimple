6af22334fc21c73186ec0dd76ec41b77a97f31cb
Ezequiel <eezquieloelero@gmail.com>
2023-04-24 23:01:46 -0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,13 @@
 package org.example;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        int length = input.length();
-        if (length > 100) {
-            System.out.println("HARD");
-        } else {
-            System.out.println("EASY");
-        }
-        scanner.close();
+
     }
 }
